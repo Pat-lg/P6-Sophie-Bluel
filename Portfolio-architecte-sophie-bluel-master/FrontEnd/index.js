7b00ebf904393aa6c88b376e0b97091ab616e221
Patrice Le Guen <patlg.dev@gmail.com>
2023-12-07 00:27:21 +0100

    créer le HTML de la galerie en JS

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/index.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/index.js
@@ -1,21 +1,41 @@
+const url = "http://localhost:5678/api/works";
+const divGallery = document.querySelector(".gallery")
+
 /* récupération des données via l'API */
 
-//  Url de l'API
-const url = "http://localhost:5678/api/works";
-
-
-const getData = async() => {
+const getData = async () => {
   try {
     const response = await fetch(url);
     if (!response.ok) throw new Error("not a valid response")
     const data = await response.json();
+  console.log(data);
     return data;
   }
   catch (err) {
     console.warn(err.message);
   }
 }
-getData().then((completeData)=> {
-  console.log(completeData);
-})
- 
+
+/* créer le HTML de la galerie */
+
+getData()
+
+  .then((completeData) => {
+    
+    // créer les balises figure+ img + figurecaption
+    completeData.forEach(objet => {
+      const figure = document.createElement("figure");
+
+      const image = document.createElement("img");
+      image.src = objet.imageUrl;
+
+      const figCaption = document.createElement("figurecaption");
+      figCaption.textContent = objet.title;
+
+    // ajout des balises figure + enfants ( img; figurecaption) dans la div .gallery
+      divGallery.appendChild(figure);
+      figure.appendChild(image);
+      figure.appendChild(figCaption);
+     
+    });
+  })
