2253279226354da7e0e3ddbdd3a5eefc2cf4af75
Patrice Le Guen <patlg.dev@gmail.com>
2023-12-11 18:28:43 +0100

    ajout des boutons-filtres avec JS

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/index.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/index.js
@@ -1,5 +1,4 @@
 const urlWorks = "http://localhost:5678/api/works";
-const divGallery = document.querySelector(".gallery")
 
 /* récupération des données Works via l'API */
 
@@ -8,7 +7,7 @@ const getDataWorks = async () => {
     const response = await fetch(urlWorks);
     if (!response.ok) throw new Error("not a valid response")
     const data = await response.json();
-    console.log(data);
+    console.table(data);
     return data;
   }
   catch (err) {
@@ -18,39 +17,40 @@ const getDataWorks = async () => {
 
 /* créer le HTML de la galerie */
 
-getDataWorks()
+const divGallery = document.querySelector(".gallery")
 
-  .then((completeData) => {
+const getdata = getDataWorks();
 
-    // créer les balises figure+ img + figurecaption
-    completeData.forEach(objet => {
-      const figure = document.createElement("figure");
+getdata.then((completeData) => {
 
-      const image = document.createElement("img");
-      image.src = objet.imageUrl;
+  // créer les balises figure+ img + figurecaption
+  completeData.forEach(objet => {
+    const figure = document.createElement("figure");
 
-      const figCaption = document.createElement("figurecaption");
-      figCaption.textContent = objet.title;
+    const image = document.createElement("img");
+    image.src = objet.imageUrl;
 
-      // ajout des balises figure + enfants ( img; figurecaption) dans la div .gallery
-      divGallery.appendChild(figure);
-      figure.appendChild(image);
-      figure.appendChild(figCaption);
+    const figCaption = document.createElement("figurecaption");
+    figCaption.innerHTML = objet.title;
 
-    });
-  })
+    // ajout des balises figure + enfants ( img; figurecaption) dans la div .gallery
+    divGallery.appendChild(figure);
+    figure.appendChild(image);
+    figure.appendChild(figCaption);
+
+  });
+})
 
 /* récupération des données Categories via l'API */
 
 const urlCategories = "http://localhost:5678/api/categories";
-console.log(urlCategories);
 
 const getDataCategories = async () => {
   try {
     const response = await fetch(urlCategories);
     if (!response.ok) throw new Error("not a valid response")
     const dataCategories = await response.json();
-    console.log(dataCategories);
+    console.table(dataCategories);
     return dataCategories;
   }
   catch (err) {
@@ -58,4 +58,24 @@ const getDataCategories = async () => {
   }
 }
 
-getDataCategories();
+/* créer les boutons-filtres */
+
+const allButtons = document.querySelector(".all-buttons");
+
+
+// button.classList.add("btn-filter");
+
+getDataCategories()
+
+  .then((dataCategories) => {
+
+    dataCategories.forEach(filterButton => {
+      // créer les boutons + noms + class btn-filter
+      const button = document.createElement("button");
+      button.textContent = filterButton.name;
+      button.classList.add("btn-filter");
+      // ajout des boutons ds la div all-buttons
+      allButtons.appendChild(button);
+    });
+  })
+
