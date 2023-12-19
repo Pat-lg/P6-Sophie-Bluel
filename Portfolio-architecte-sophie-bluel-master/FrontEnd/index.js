118bdf3e90adfba99c30558a89742b8233f5e9ba
Patrice Le Guen <patlg.dev@gmail.com>
2023-12-19 16:06:20 +0100

    filtrer la galerie par  catégorie (sauf bouton selected)

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/index.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/index.js
@@ -40,7 +40,7 @@ dataWork.then((completeData) => {
 
   });
 })
-
+console.log(dataWork);
 /* récupération des données Categories via l'API */
 
 const urlCategories = "http://localhost:5678/api/categories";
@@ -62,19 +62,85 @@ const getDataCategories = async () => {
 
 const allButtons = document.querySelector(".all-buttons");
 
-
 const dataCategories = getDataCategories();
 
-  dataCategories.then((dataCategories) => {
+dataCategories.then((dataCategories) => {
 
-    dataCategories.forEach(filterButton => {
+  dataCategories.forEach(filterButton => {
 
-      // créer les boutons + noms + class btn-filter
-      const button = document.createElement("button");
-      button.textContent = filterButton.name;
-      button.classList.add("btn-filter");
+    // créer les boutons + noms + id + class btn-filter 
+    const button = document.createElement("button");
+    button.textContent = filterButton.name;
+    button.id = filterButton.id;
+    button.classList.add("btn-filter");
 
-      // ajout des boutons ds la div all-buttons
-      allButtons.appendChild(button);
+    // ajout des boutons ds la div all-buttons
+    allButtons.appendChild(button);
+  });
+})
+
+/* filtrer les catégories */
+
+const sortBtn = async () => {
+  // récuperer les données Works
+  const retreiveDataWorks = await getDataWorks();
+  // sélectionner tous les boutons-filtres
+  const allBtns = document.querySelectorAll(".all-buttons button");
+
+  allBtns.forEach((button) => {
+    // retirer la classe selected des boutons-filtres
+    button.classList.remove("selected");
+
+    button.addEventListener("click", (e) => {
+      // sélectionner les id des boutons
+      const idBtn = e.target.id;
+      button.classList.add("selected");
+      // RAZ de la galerie d'images au clic
+      divGallery.innerHTML = "";
+
+      if (idBtn !== "0") {
+        const sortedCategories = retreiveDataWorks.filter((element) => {
+          
+          return element.categoryId == idBtn;
+        });
+        sortedCategories.forEach(category => {
+
+          const figure = document.createElement("figure");
+
+          const image = document.createElement("img");
+          image.src = category.imageUrl;
+      
+          const figCaption = document.createElement("figurecaption");
+          figCaption.innerHTML = category.title;
+      
+          // ajout des balises figure + enfants ( img; figurecaption) dans la div .gallery
+          divGallery.appendChild(figure);
+          figure.appendChild(image);
+          figure.appendChild(figCaption);;
+        });
+      } else {
+        dataWork.then((completeData) => {
+
+          // créer les balises figure+ img + figurecaption
+          completeData.forEach(objet => {
+            
+            const figure = document.createElement("figure");
+        
+            const image = document.createElement("img");
+            image.src = objet.imageUrl;
+        
+            const figCaption = document.createElement("figurecaption");
+            figCaption.innerHTML = objet.title;
+        
+            // ajout des balises figure + enfants ( img; figurecaption) dans la div .gallery
+            divGallery.appendChild(figure);
+            figure.appendChild(image);
+            figure.appendChild(figCaption);
+        
+          });
+        });
+      }
     });
-  })
+  });
+}
+sortBtn();
